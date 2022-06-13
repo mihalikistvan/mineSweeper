9d1951d23fa3d10e88e97cdf88051ed7afb0c877
mihalikistvan <mihalikistvan@github.com>
2022-06-13 12:23:54 +0200

    prettier code

--- main.py
+++ main.py
@@ -1,75 +1,81 @@
 import random
 
+
 def printTable(tableDict):
-   try:
-      return "+-+-+-+ \n" \
-             f"|{tableDict['A1']}|{tableDict['A2']}|{tableDict['A3']}| \n" \
-             "+-+-+-+ \n" \
-             f"|{tableDict['B1']}|{tableDict['B2']}|{tableDict['B3']}| \n" \
-             "+-+-+-+ \n" \
-             f"|{tableDict['C1']}|{tableDict['C2']}|{tableDict['C3']}| \n" \
-             "+-+-+-+"
-   except:
-      return 'wrong input'
+    try:
+        return "+-+-+-+ \n" \
+               f"|{tableDict['A1']}|{tableDict['A2']}|{tableDict['A3']}| \n" \
+               "+-+-+-+ \n" \
+               f"|{tableDict['B1']}|{tableDict['B2']}|{tableDict['B3']}| \n" \
+               "+-+-+-+ \n" \
+               f"|{tableDict['C1']}|{tableDict['C2']}|{tableDict['C3']}| \n" \
+               "+-+-+-+"
+    except:
+        return 'wrong input'
+
 
 def setupBombs(numOfBombs):
-   if isinstance(numOfBombs, int):
-      possibleBombs = ['A1','A2','A3','B1','B2','B3','C1','C2','C3']
-      bombPlacement = []
-      if numOfBombs <= len(possibleBombs):
-         for i in range (0,numOfBombs):
-            bomb = random.choice(possibleBombs)
-            possibleBombs.remove(bomb)
-            bombPlacement.append(bomb)
-         return bombPlacement
-   return 'wrong input'
+    if isinstance(numOfBombs, int):
+        possibleBombs = ['A1', 'A2', 'A3', 'B1', 'B2', 'B3', 'C1', 'C2', 'C3']
+        bombPlacement = []
+        if numOfBombs <= len(possibleBombs):
+            for i in range(0, numOfBombs):
+                bomb = random.choice(possibleBombs)
+                possibleBombs.remove(bomb)
+                bombPlacement.append(bomb)
+            return bombPlacement
+    return 'wrong input'
 
-def decideIfBombOrPoints(coordinate,bombPlacement):
-   if isinstance(coordinate, str) and isinstance(bombPlacement, list):
-      return coordinate in bombPlacement
-   return False
 
-def decidePoints(coordinate,bombPlacement):
-    bombPossiblities= {
-        'A1':['A2','B1','B2'],'A2':['A1','A3','B1','B2','B3'],'A3':['A2','B2','B3'],
-        'B1':['A1','A2','B2','C1','C2'],'B2':['A1','A2','A3','B1','B3','C1','C2','C3'],'B3':['A2','A3','B2','C2','C3'],
-        'C1':['B1','B2','C2'],'C2':['B1','B2','B3','C1','C3'],'C3':['B2','B3','C2']
+def decideIfBombOrPoints(coordinate, bombPlacement):
+    if isinstance(coordinate, str) and isinstance(bombPlacement, list):
+        return coordinate in bombPlacement
+    return False
+
+
+def decidePoints(coordinate, bombPlacement):
+    bombPossiblities = {
+        'A1': ['A2', 'B1', 'B2'], 'A2': ['A1', 'A3', 'B1', 'B2', 'B3'], 'A3': ['A2', 'B2', 'B3'],
+        'B1': ['A1', 'A2', 'B2', 'C1', 'C2'], 'B2': ['A1', 'A2', 'A3', 'B1', 'B3', 'C1', 'C2', 'C3'], 'B3': ['A2', 'A3', 'B2', 'C2', 'C3'],
+        'C1': ['B1', 'B2', 'C2'], 'C2': ['B1', 'B2', 'B3', 'C1', 'C3'], 'C3': ['B2', 'B3', 'C2']
     }
     return (len(list(set(bombPossiblities[coordinate]).intersection(bombPlacement))))
 
+
 def game():
-   viewedTable= {
-        'A1':' ','A2':' ','A3':' ',
-        'B1':' ','B2':' ','B3':' ',
-        'C1':' ','C2':' ','C3':' '
-   }
-   print (printTable(viewedTable))
-   fullTable =['A1','A2','A3','B1','B2','B3','C1','C2','C3']
-   bombPlacement = setupBombs(3)
-   
-   remainingPlaces = fullTable
-   nonBomblist  = [x for x in fullTable if (x not in bombPlacement)]
-   
-   gameRunning = True
-   while gameRunning:
-      choice = random.choice(remainingPlaces)
-      remainingPlaces.remove(choice)
+    viewedTable = {
+        'A1': ' ', 'A2': ' ', 'A3': ' ',
+        'B1': ' ', 'B2': ' ', 'B3': ' ',
+        'C1': ' ', 'C2': ' ', 'C3': ' '
+    }
+    print(printTable(viewedTable))
+    fullTable = ['A1', 'A2', 'A3', 'B1', 'B2', 'B3', 'C1', 'C2', 'C3']
+    bombPlacement = setupBombs(3)
 
-      print ('choice: ',choice)
-      
-      if decideIfBombOrPoints(choice,bombPlacement):
-         viewedTable[choice] = 'X'
-         print (printTable(viewedTable))
-         print ('game Over bombs were at:' , bombPlacement)
-         gameRunning = False
-      else:
-         nonBomblist.remove(choice)
-         viewedTable[choice] = decidePoints(choice,bombPlacement)
-         print (printTable(viewedTable))
-      if len(nonBomblist) == 0:
-         print ('You win')
-         gameRunning = False
-            
-   return 'game end'
+    remainingPlaces = fullTable
+    nonBomblist = [x for x in fullTable if (x not in bombPlacement)]
 
-print (game())
+    gameRunning = True
+    while gameRunning:
+        choice = random.choice(remainingPlaces)
+        remainingPlaces.remove(choice)
+
+        print('choice: ', choice)
+
+        if decideIfBombOrPoints(choice, bombPlacement):
+            viewedTable[choice] = 'X'
+            print(printTable(viewedTable))
+            print('game Over bombs were at:', bombPlacement)
+            gameRunning = False
+        else:
+            nonBomblist.remove(choice)
+            viewedTable[choice] = decidePoints(choice, bombPlacement)
+            print(printTable(viewedTable))
+        if len(nonBomblist) == 0:
+            print('You win')
+            gameRunning = False
+
+    return 'game end'
+
+
+print(game())
--- test_main.py
+++ test_main.py
@@ -1,65 +1,65 @@
-from main import * 
+from main import *
+
 
 def test():
-    ### game
+    # game
     assert game() == "game end"
 
-    
-    ### table printing
-    viewedTable0= ''
-    viewedTable1= {
-            'A1':' ','A2':' ','A3':' '
-        }
-    viewedTable2= {
-        'A1':' ','A2':' ','A3':' ',
-        'B1':' ','B2':' ','B3':' ',
-        'C1':' ','C2':' ','C3':' '
+    # table printing
+    viewedTable0 = ''
+    viewedTable1 = {
+        'A1': ' ', 'A2': ' ', 'A3': ' '
     }
-    viewedTable3= {
-        'A1':'1','A2':'1','A3':'1',
-        'B1':'1','B2':' ','B3':'1',
-        'C1':'1','C2':'1','C3':'1'
+    viewedTable2 = {
+        'A1': ' ', 'A2': ' ', 'A3': ' ',
+        'B1': ' ', 'B2': ' ', 'B3': ' ',
+        'C1': ' ', 'C2': ' ', 'C3': ' '
+    }
+    viewedTable3 = {
+        'A1': '1', 'A2': '1', 'A3': '1',
+        'B1': '1', 'B2': ' ', 'B3': '1',
+        'C1': '1', 'C2': '1', 'C3': '1'
     }
     assert printTable(viewedTable0) == 'wrong input'
     assert printTable(viewedTable1) == 'wrong input'
     assert printTable(viewedTable2) == "+-+-+-+ \n" \
-                                        f"| | | | \n" \
-                                        "+-+-+-+ \n" \
-                                        f"| | | | \n" \
-                                        "+-+-+-+ \n" \
-                                        f"| | | | \n" \
-                                        "+-+-+-+"
+        f"| | | | \n" \
+        "+-+-+-+ \n" \
+        f"| | | | \n" \
+        "+-+-+-+ \n" \
+        f"| | | | \n" \
+        "+-+-+-+"
     assert printTable(viewedTable3) == "+-+-+-+ \n" \
-                                        f"|1|1|1| \n" \
-                                        "+-+-+-+ \n" \
-                                        f"|1| |1| \n" \
-                                        "+-+-+-+ \n" \
-                                        f"|1|1|1| \n" \
-                                        "+-+-+-+"
+        f"|1|1|1| \n" \
+        "+-+-+-+ \n" \
+        f"|1| |1| \n" \
+        "+-+-+-+ \n" \
+        f"|1|1|1| \n" \
+        "+-+-+-+"
 
-    ### bomb setup
-    setup0 =setupBombs('testForWrongInput')
-    setup1 =setupBombs(1)
-    setup2 =setupBombs(3)
-    setup3 =setupBombs(8)
-    setup4 =setupBombs(12)
+    # bomb setup
+    setup0 = setupBombs('testForWrongInput')
+    setup1 = setupBombs(1)
+    setup2 = setupBombs(3)
+    setup3 = setupBombs(8)
+    setup4 = setupBombs(12)
 
-    assert setup0  == 'wrong input'
-    assert len(setup1)  == 1
-    assert len(setup2)  == 3
-    assert len(setup2)  == len(set(setup2))
-    assert len(setup3)  == 8
-    assert len(setup3)  == len(set(setup3))
-    assert setup4  == 'wrong input'
+    assert setup0 == 'wrong input'
+    assert len(setup1) == 1
+    assert len(setup2) == 3
+    assert len(setup2) == len(set(setup2))
+    assert len(setup3) == 8
+    assert len(setup3) == len(set(setup3))
+    assert setup4 == 'wrong input'
 
-    ### point system
-    assert decideIfBombOrPoints('A2',['A2','B1','B2']) == True
-    assert decideIfBombOrPoints('A1',['A2','B1','B2']) == False
-    assert decideIfBombOrPoints('A1',[]) == False
-    assert decideIfBombOrPoints(12,['A2','B1','B2']) == False
-    assert decideIfBombOrPoints('A2',['A2']) == True
+    # point system
+    assert decideIfBombOrPoints('A2', ['A2', 'B1', 'B2']) == True
+    assert decideIfBombOrPoints('A1', ['A2', 'B1', 'B2']) == False
+    assert decideIfBombOrPoints('A1', []) == False
+    assert decideIfBombOrPoints(12, ['A2', 'B1', 'B2']) == False
+    assert decideIfBombOrPoints('A2', ['A2']) == True
 
-    assert decidePoints('A1',['A2','B1','B2']) == 3
-    assert decidePoints('A2',['C1','C2','C3']) == 0
-    assert decidePoints('B2',['A1','C2','C3']) == 3
-    assert decidePoints('B2',['C1','C2','C3']) == 3
+    assert decidePoints('A1', ['A2', 'B1', 'B2']) == 3
+    assert decidePoints('A2', ['C1', 'C2', 'C3']) == 0
+    assert decidePoints('B2', ['A1', 'C2', 'C3']) == 3
+    assert decidePoints('B2', ['C1', 'C2', 'C3']) == 3
